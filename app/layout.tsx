0b9e87f4cba8a291b5b563f118b03fdf161171f3
nupursol <nupur@bu.edu>
2025-04-30 21:25:33 -0400

    fixed bug in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en"> {/* language to English */}
+        <html lang="en">
         <body>
         <Providers> {/*wrapping children with Providers to provide context*/}
             {children} {/* rendering the page content (children passed to this layout) */}
